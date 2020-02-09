80f66f2478ccb9ff226d2d3cca27129756a5e82d
costigt <costigt@tcd.ie>
2020-02-09 15:12:30 +0000

    started mapping out the io ports

--- IOPorts.cpp
+++ IOPorts.cpp
@@ -3,11 +3,35 @@
 #include "Exceptions.hpp"
 #include <typeinfo.h>
 
-constexpr unsigned int IO_START = 0x1f801000;
-constexpr unsigned int GP0_Send = 0x1f801810 - IO_START;
-constexpr unsigned int GP1_Send = 0x1f801814 - IO_START;
-constexpr unsigned int GPUREAD = 0x1f801810 - IO_START;
-constexpr unsigned int GPUSTAT = 0x1f801814 - IO_START;
+constexpr unsigned int IO_START = 0x1F801000;
+constexpr unsigned int GP0_Send_GPUREAD = 0x1f801810 - IO_START;
+constexpr unsigned int GP1_Send_GPUSTAT = 0x1f801814 - IO_START;
+
+constexpr unsigned int MEMORY_CONTROL_1_START = 0x1F801000 - IO_START;
+constexpr unsigned int MEMORY_CONTROL_1_END = MEMORY_CONTROL_1_START + MEMORY_CONTROL_1_SIZE;
+
+constexpr unsigned int MEMORY_CONTROL_2_START = 0x1F801060 - IO_START;
+constexpr unsigned int MEMORY_CONTROL_2_END = MEMORY_CONTROL_2_START + MEMORY_CONTROL_2_SIZE;
+
+constexpr unsigned int SPU_CONTROL_START = 0x1F801D80 - IO_START;
+constexpr unsigned int SPU_CONTROL_END = SPU_CONTROL_START + SPU_CONTROL_SIZE;
+
+constexpr unsigned int SPU_VOICE_START = 0x1F801C00 - IO_START;
+constexpr unsigned int SPU_VOICE_END = SPU_VOICE_START + SPU_VOICE_SIZE;
+
+constexpr unsigned int POST = 0x1F802041 - IO_START;
+
+constexpr unsigned int I_STAT_START = 0x1F801070 - IO_START;
+constexpr unsigned int I_STAT_END = I_STAT_START + I_STAT_SIZE;
+
+constexpr unsigned int I_MASK_START = 0x1F801074 - IO_START;
+constexpr unsigned int I_MASK_END = I_MASK_START + I_MASK_SIZE;
+
+constexpr unsigned int TIMER_START = 0x1F801100 - IO_START;
+constexpr unsigned int TIMER_END = TIMER_START + TIMER_SIZE;
+
+constexpr unsigned int DMA_START = 0x1F801080 - IO_START;
+constexpr unsigned int DMA_END = DMA_START + DMA_SIZE;
 
 void IOPorts::init(std::shared_ptr<Gpu> _gpu)
 {
@@ -16,54 +40,74 @@ void IOPorts::init(std::shared_ptr<Gpu> _gpu)
 
 unsigned char* IOPorts::get(unsigned int address, bool read)
 {
-	if (read)
+	if (address == GP0_Send_GPUREAD)
 	{
-		return read_access(address);
-	}
-	else
-	{
-		return write_access(address);
-	}
-}
-
-unsigned char* IOPorts::read_access(unsigned int addr)
-{
-	switch (addr)
-	{
-		case GPUREAD:
+		if (read)
 		{
 			return gpu->GPUREAD.byte_value;
-		} break;
-
-		case GPUSTAT:
+		}
+		else
+		{
+			return gpu->GP0_send.byte_value;
+		}
+	}
+	else if (address == GP1_Send_GPUSTAT)
+	{
+		if (read)
 		{
 			return gpu->GPUSTAT.byte_value;
 		}
-
-		default:
-		{
-			throw bus_error();
-		}
-	}
-}
-
-unsigned char* IOPorts::write_access(unsigned int addr)
-{
-	switch (addr)
-	{
-		case GP0_Send:
-		{
-			return gpu->GP0_send.byte_value;
-		} break;
-
-		case GP1_Send:
+		else
 		{
 			return gpu->GP1_send.byte_value;
-		} break;
-
-		default:
-		{
-			throw bus_error();
 		}
+	}
+	else if (address >= MEMORY_CONTROL_1_START &&
+		address < MEMORY_CONTROL_1_END)
+	{
+		return &memory_control_1[address - MEMORY_CONTROL_1_START];
+	}
+	else if (address >= MEMORY_CONTROL_2_START &&
+		address < MEMORY_CONTROL_2_END)
+	{
+		return &memory_control_2[address - MEMORY_CONTROL_2_START];
+	}
+	else if (address >= SPU_CONTROL_START &&
+		address < SPU_CONTROL_END)
+	{
+		return &spu_control[address - SPU_CONTROL_START];
+	}
+	else if (address >= SPU_VOICE_START &&
+		address < SPU_VOICE_END)
+	{
+		return &spu_voice_registers[address - SPU_VOICE_START];
+	}
+	else if (address >= I_STAT_START &&
+		address < I_STAT_END)
+	{
+		return &i_stat[address - I_STAT_START];
+	}
+	else if (address >= I_MASK_START &&
+		address < I_MASK_END)
+	{
+		return &i_mask[address - I_MASK_START];
+	}
+	else if (address >= TIMER_START &&
+		address < TIMER_END)
+	{
+		return &timers[address - TIMER_START];
+	}
+	else if (address >= DMA_START &&
+		address < DMA_END)
+	{
+		return &dma_registers[address - DMA_START];
+	}
+	else if (address == POST)
+	{
+		return &post;
+	}
+	else
+	{
+		throw bus_error();
 	}
 }
--- IOPorts.hpp
+++ IOPorts.hpp
@@ -1,6 +1,23 @@
 #include <unordered_map>
 #include <memory>
 
+// https://problemkaputt.de/psx-spx.htm#memorymap
+constexpr unsigned int MEMORY_CONTROL_1_SIZE = 36;
+constexpr unsigned int MEMORY_CONTROL_2_SIZE = 4;
+constexpr unsigned int PERIPHERAL_IO_SIZE = 30;
+
+constexpr unsigned int SPU_CONTROL_SIZE = 64;
+constexpr unsigned int SPU_VOICE_SIZE = 576;
+
+// I_STAT_SIZE and I_MASK_SIZE only use the first 2 bytes
+// and the next 2 in both are considered garbage areas
+constexpr unsigned int I_STAT_SIZE = 4;
+constexpr unsigned int I_MASK_SIZE = 4;
+
+constexpr unsigned int TIMER_SIZE = 45;
+
+constexpr unsigned int DMA_SIZE = 128;
+
 class Gpu;
 class IOPorts
 {
@@ -11,7 +28,20 @@ public:
 
 private:
 	std::shared_ptr<Gpu> gpu = nullptr;
-	// some of the memory registers return different values when read or written too
-	unsigned char* read_access(unsigned int addr);
-	unsigned char* write_access(unsigned int addr);
+
+	unsigned char memory_control_1[MEMORY_CONTROL_1_SIZE] = {0};
+	unsigned char memory_control_2[MEMORY_CONTROL_2_SIZE] = {0};
+	unsigned char peripheral_io[PERIPHERAL_IO_SIZE] = { 0 };
+
+	unsigned char spu_control[SPU_CONTROL_SIZE] = { 0 };
+	unsigned char spu_voice_registers[SPU_VOICE_SIZE] = { 0 };
+	
+	unsigned char post = 0;
+	
+	unsigned char i_stat[I_STAT_SIZE] = { 0 };
+	unsigned char i_mask[I_MASK_SIZE] = { 0 };
+
+	unsigned char timers[TIMER_SIZE] = { 0 };
+
+	unsigned char dma_registers[DMA_SIZE] = { 0 };
 };
