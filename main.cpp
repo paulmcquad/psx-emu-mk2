791e2a1c354d6e6c42417c4e0257e5169c59af0c
costigt <costigt@tcd.ie>
2020-02-09 20:38:36 +0000

    added hello world triangle

--- main.cpp
+++ main.cpp
@@ -9,6 +9,18 @@
 
 constexpr double FRAME_TIME_SECS = 1.0 / 60.0;
 
+static const char* vertex_shader_src =
+"#version 150 core\n"
+"in vec2 position;\n"
+"void main(){\n"
+"gl_Position = vec4(position, 0.0, 1.0);}\n";
+
+static const char* frag_shader_src =
+"#version 150 core\n"
+"out vec4 outColor;\n"
+"void main(){\n"
+"outColor = vec4(1.0, 1.0, 1.0, 1.0);}\n";
+
 void key_callback(GLFWwindow * window, int key, int /*scancode*/, int action, int /*mods*/)
 {
 	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
@@ -53,6 +65,44 @@ int main(int num_args, char ** args )
 		return -1;
 	}
 
+	std::cout << "Setting up OpenGL\n";
+	GLuint vao, vbo, vert_shader, frag_shader, shader_program;
+	{
+		glGenVertexArrays(1, &vao);
+		glBindVertexArray(vao);
+
+		glGenBuffers(1, &vbo);
+
+		float vertices[] = {
+			0.0f,  0.5f,
+			0.5f, -0.5f,
+		   -0.5f, -0.5f
+		};
+
+		glBindBuffer(GL_ARRAY_BUFFER, vbo);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+		vert_shader = glCreateShader(GL_VERTEX_SHADER);
+		glShaderSource(vert_shader, 1, &vertex_shader_src, nullptr);
+		glCompileShader(vert_shader);
+
+		frag_shader = glCreateShader(GL_FRAGMENT_SHADER);
+		glShaderSource(frag_shader, 1, &frag_shader_src, nullptr);
+		glCompileShader(frag_shader);
+
+		shader_program = glCreateProgram();
+		glAttachShader(shader_program, vert_shader);
+		glAttachShader(shader_program, frag_shader);
+
+		glLinkProgram(shader_program);
+		glUseProgram(shader_program);
+
+		GLint pos_attrib = glGetAttribLocation(shader_program, "position");
+		glVertexAttribPointer(pos_attrib, 2, GL_FLOAT, GL_FALSE, 0, 0);
+
+		glEnableVertexAttribArray(pos_attrib);
+	}
+
 	// Device I/O
 	std::cout << "Creating GPU\n";
 	std::shared_ptr<Gpu> gpu = std::make_shared<Gpu>();
@@ -88,6 +138,11 @@ int main(int num_args, char ** args )
 
 		if (current_frame_time >= FRAME_TIME_SECS)
 		{
+			glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+			glClear(GL_COLOR_BUFFER_BIT);
+
+			glDrawArrays(GL_TRIANGLES, 0, 3);
+
 			glfwSwapBuffers(window);
 			glfwPollEvents();
 			current_frame_time = 0;
@@ -95,6 +150,12 @@ int main(int num_args, char ** args )
 	}
 
 	// cleanup
+	glDeleteProgram(shader_program);
+	glDeleteShader(frag_shader);
+	glDeleteShader(vert_shader);
+	glDeleteBuffers(1, &vbo);
+	glDeleteVertexArrays(1, &vao);
+
 	glfwDestroyWindow(window);
 	glfwTerminate();
 
