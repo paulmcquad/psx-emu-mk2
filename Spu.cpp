854ba7b53519946ec200aca9d5d6727f7e907c09
costigt <costigt@tcd.ie>
2020-05-18 11:34:08 +0100

    removed SDL2 since i'm not using it yet

--- Spu.cpp
+++ Spu.cpp
@@ -1,37 +1,12 @@
 #include <stdexcept>
 #include "Spu.hpp"
-#include "SDL.h"
 
 constexpr unsigned int SPU_START = 0x1F801C00;
 constexpr unsigned int SPU_END = SPU_START + SPU_VOICE_SIZE + SPU_CONTROL_SIZE;
 
-static SDL_AudioDeviceID device_id;
-
-//https://gigi.nullneuron.net/gigilabs/playing-a-wav-file-using-sdl2/
-//https://gist.github.com/armornick/3447121
-//https://github.com/leixiaohua1020/simplest_media_play/blob/master/simplest_audio_play_sdl2/simplest_audio_play_sdl2.cpp
 bool Spu::init()
 {
 	spu_ram = new unsigned char[512 * 1042];
-
-	if (SDL_Init(SDL_INIT_AUDIO) < 0)
-	{
-		return false;
-	}
-
-	SDL_AudioSpec psx_audio_spec;
-	psx_audio_spec.freq = 44100;
-	psx_audio_spec.format = AUDIO_S16SYS;
-	psx_audio_spec.channels = 2;
-	psx_audio_spec.silence = 0;
-	psx_audio_spec.samples = 1024;
-	psx_audio_spec.callback = nullptr;
-
-	SDL_AudioDeviceID device_id = SDL_OpenAudioDevice(nullptr, 0, &psx_audio_spec, nullptr, 0);
-	SDL_PauseAudioDevice(device_id, 0);
-
-	initialized = true;
-
 	return true;
 }
 
@@ -40,12 +15,6 @@ Spu::~Spu()
 	if (spu_ram)
 	{
 		delete[] spu_ram;
-	}
-
-	if (initialized)
-	{
-		SDL_CloseAudioDevice(device_id);
-		SDL_Quit();
 	}
 }
 
